fbc628e4db8cfe052ff5bcb6d24f1abe91ad8921
Simon Moradbakti <simonmor@kth.se>
2025-04-30 19:09:37 +0200

    Update AmountDTO.java

--- Sem3/retailStore/src/se/kth/iv1350/retailStore/dto/AmountDTO.java
+++ Sem3/retailStore/src/se/kth/iv1350/retailStore/dto/AmountDTO.java
@@ -2,24 +2,24 @@ package se.kth.iv1350.retailStore.dto;
 
 public class AmountDTO {
 
-	private final float amount;
+    private final float amount;
 
-	/**
-	 * Skapar ett nytt objekt som innehåller ett visst belopp.
-	 * 
-	 * @param amount Det belopp som ska sparas.
-	 */
-	public AmountDTO(float amount) {
-		this.amount = amount;
-	}
+    /**
+     * Creates a new object that contains some amount.
+     * 
+     * @param amount The amount to be stored.
+     */
+    public AmountDTO(float amount) {
+        this.amount = amount;
+    }
 
-	/**
-	 * Returnerar det sparade beloppet.
-	 * 
-	 * @return Beloppet som ett float-värde.
-	 */
-	public float getAmount() {
-		return amount;
-	}
+    /**
+     * Returns the stored amount.
+     * 
+     * @return The amount as a float value.
+     */
+    public float getAmount() {
+        return amount;
+    }
 
 }
