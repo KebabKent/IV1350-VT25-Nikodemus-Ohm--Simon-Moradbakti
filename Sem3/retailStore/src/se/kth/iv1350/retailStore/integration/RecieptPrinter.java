4952562539d3c5ff65d9a0fa8928f64b5aa5de0d
Simon Moradbakti <simonmor@kth.se>
2025-04-30 18:55:33 +0200

    Update RecieptPrinter.java

--- Sem3/retailStore/src/se/kth/iv1350/retailStore/integration/RecieptPrinter.java
+++ Sem3/retailStore/src/se/kth/iv1350/retailStore/integration/RecieptPrinter.java
@@ -6,6 +6,15 @@ import se.kth.iv1350.retailStore.dto.SaleDTO;
 
 public class RecieptPrinter {
 
+	/**
+	 * Prints the receipt for a completed sale. The receipt includes:
+	 * The time of the sale and when it ended
+	 * A list of items sold, with details such as ID, name, quantity, price, VAT, and total price for each item
+	 * The total price, VAT, VAT percentage, discounted price, and discount percentage
+	 * The amount paid by the customer and the change given back
+	 *
+	 * @param saleDTO The sale data object containing all information needed to print the receipt.
+	 */
 	public static void printReciept(SaleDTO saleDTO) {
 		System.out.println("********** Receipt **********");
 
@@ -44,7 +53,6 @@ public class RecieptPrinter {
 		System.out.println("Amount paid: " +
 				saleDTO.getAmountPaid().getAmount());
 		System.out.println("Change: " + saleDTO.getChange().getAmount());
-
 	}
 
 }
