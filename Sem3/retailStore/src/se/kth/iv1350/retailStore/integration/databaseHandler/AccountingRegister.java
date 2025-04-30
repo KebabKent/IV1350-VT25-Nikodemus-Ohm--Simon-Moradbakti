153b0be019e5da7981b7eeff81369c31a859d49b
Simon Moradbakti <simonmor@kth.se>
2025-04-30 18:51:46 +0200

    Update AccountingRegister.java

--- Sem3/retailStore/src/se/kth/iv1350/retailStore/integration/databaseHandler/AccountingRegister.java
+++ Sem3/retailStore/src/se/kth/iv1350/retailStore/integration/databaseHandler/AccountingRegister.java
@@ -9,10 +9,21 @@ public class AccountingRegister {
 
 	private List<SaleDTO> salesHistory;
 
+	/**
+	 * Creates a new accounting register that keeps track of all completed sales.
+	 * It stores them in a list which represents the accounting history.
+	 */
 	AccountingRegister() {
 		this.salesHistory = new ArrayList<>();
 	}
 
+	/**
+	 * Adds a completed sale to the accounting history.
+	 * This simulates saving the sale to an external accounting system.
+	 * Also prints all sale IDs stored so far (for testing/debugging).
+	 *
+	 * @param sale The sale to be recorded in the register.
+	 */
 	void updateRegister(SaleDTO sale) {
 		this.salesHistory.add(sale);
 
