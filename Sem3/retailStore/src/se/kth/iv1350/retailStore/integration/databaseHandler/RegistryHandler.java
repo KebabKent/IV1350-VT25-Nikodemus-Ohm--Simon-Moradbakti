b8811cb9ff0cf7623995361997c3c705ac79ae23
Simon Moradbakti <simonmor@kth.se>
2025-04-30 18:54:26 +0200

    Update RegistryHandler.java

--- Sem3/retailStore/src/se/kth/iv1350/retailStore/integration/databaseHandler/RegistryHandler.java
+++ Sem3/retailStore/src/se/kth/iv1350/retailStore/integration/databaseHandler/RegistryHandler.java
@@ -8,16 +8,33 @@ public class RegistryHandler {
 	private AccountingRegister accountingRegister;
 	private InventoryRegister inventoryRegister;
 
+	/**
+	 * Creates a new RegistryHandler that manages both the accounting and inventory registers.
+	 * This is the main entry point for interacting with the registers.
+	 */
 	public RegistryHandler() {
 		this.accountingRegister = new AccountingRegister();
 		this.inventoryRegister = new InventoryRegister();
 	}
 
+	/**
+	 * Updates both the accounting and inventory registers with information from a completed sale.
+	 * This ensures that the sale is logged in the accounting system and the inventory is updated.
+	 *
+	 * @param sale The sale that has been completed and needs to be recorded.
+	 */
 	public void updateRegisters(SaleDTO sale) {
 		accountingRegister.updateRegister(sale);
 		inventoryRegister.updateRegister(sale);
 	}
 
+	/**
+	 * Retrieves information about a specific item from the inventory.
+	 * It searches the inventory register to find the item based on the provided information.
+	 *
+	 * @param searchedItem The item to be searched in the inventory.
+	 * @return The item found in the inventory, or null if not found.
+	 */
 	public ItemDTO retrieveItemInfo(ItemDTO searchedItem) {
 
 		ItemDTO foundItem = inventoryRegister.retrieveItemInfo(searchedItem);
@@ -25,10 +42,16 @@ public class RegistryHandler {
 		return foundItem;
 	}
 
+	/**
+	 * Fetches the discount percentage for a customer based on their customer ID.
+	 * If the customer is recognized (e.g., customer ID "123"), they receive a discount.
+	 *
+	 * @param customerId The ID of the customer for which the discount is to be fetched.
+	 * @return The discount percentage for the given customer.
+	 */
 	public float fetchDiscount(String customerId) {
 		float discount = DiscountRegister.fetchDiscount(customerId);
 
 		return discount;
 	}
-
 }
