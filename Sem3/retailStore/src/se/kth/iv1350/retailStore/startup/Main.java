7001fa37b997f218de39c594d8a8fb64c41eccc0
Simon Moradbakti <simonmor@kth.se>
2025-04-30 19:04:43 +0200

    Update Main.java

--- Sem3/retailStore/src/se/kth/iv1350/retailStore/startup/Main.java
+++ Sem3/retailStore/src/se/kth/iv1350/retailStore/startup/Main.java
@@ -8,11 +8,15 @@ import se.kth.iv1350.retailStore.integration.databaseHandler.RegistryHandler;
  * Main class to start the program.
  * This class is responsible for creating the necessary objects and starting the
  * program.
- * 
  */
-
 public class Main {
 
+	/**
+	 * The entry point of the program. Initializes the necessary components and
+	 * starts the sample execution.
+	 * 
+	 * @param args Command line arguments (not used).
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Retail Store!");
 
