bffa2916f96381702e4c97f25aba742f441aa391
Simon Moradbakti <simonmor@kth.se>
2025-04-30 18:57:26 +0200

    Update CashRegister.java

--- Sem3/retailStore/src/se/kth/iv1350/retailStore/model/CashRegister.java
+++ Sem3/retailStore/src/se/kth/iv1350/retailStore/model/CashRegister.java
@@ -7,10 +7,22 @@ public class CashRegister {
 
 	private AmountDTO balance;
 
+	/**
+	 * Initializes a new cash register with a starting balance of 100,000 units.
+	 * The starting balance is set when the cash register is created.
+	 */
 	public CashRegister() {
 		this.balance = new AmountDTO(100000);
 	}
 
+	/**
+	 * Registers the amount paid by the customer and the change given back. 
+	 * The cash register's balance is updated based on the amount paid and the change.
+	 * This method prints details about the transaction, including the amount paid, the change, and the current balance.
+	 *
+	 * @param saleInfo The sale data, containing the amount paid and change for the transaction.
+         * "this.balance" Updates the cash register balance
+	 */
 	public void registerAmountPayed(SaleDTO saleInfo) {
 
 		AmountDTO amountPaid = saleInfo.getAmountPaid();
