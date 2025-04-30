fe6a59117292ae2a733fea64a6647009e5423c01
Simon Moradbakti <simonmor@kth.se>
2025-04-30 19:02:37 +0200

    Update Period.java

--- Sem3/retailStore/src/se/kth/iv1350/retailStore/model/Period.java
+++ Sem3/retailStore/src/se/kth/iv1350/retailStore/model/Period.java
@@ -5,19 +5,35 @@ public class Period {
     private java.time.LocalTime saleTime;
     private java.time.LocalTime saleEndTime;
 
+    /**
+     * Constructor that initializes the sale time to the current time.
+     */
     public Period() {
         this.saleTime = java.time.LocalTime.now();
         this.saleEndTime = null;
     }
 
+    /**
+     * Sets the sale end time to the current time.
+     */
     public void setEndTime() {
         this.saleEndTime = java.time.LocalTime.now();
     }
 
+    /**
+     * Returns the sale start time.
+     * 
+     * @return The sale start time as a LocalTime object.
+     */
     public java.time.LocalTime getSaleTime() {
         return saleTime;
     }
 
+    /**
+     * Returns the sale end time.
+     * 
+     * @return The sale end time as a LocalTime object or null if not set.
+     */
     public java.time.LocalTime getSaleEndTime() {
         return saleEndTime;
     }
